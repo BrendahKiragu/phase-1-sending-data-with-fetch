ffea3c7f813325f73ef2e196a4bd79b87e3b0346
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-10 13:22:33 +0300

    creates a post request with fetch

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+//creates a function that takes in fetch() request
 function submitData (name, email){
+
+  //sends a Post request
 return fetch ('http://localhost:3000/users', {
   method: 'POST',
 
@@ -7,19 +10,23 @@ return fetch ('http://localhost:3000/users', {
     "Accept": "application/json"
   },
 
+//converts the name and email to JSON string
   body: JSON.stringify({
     name, email
   })
 })
 
+//accesses the response and converts it to js object
 .then(function(res){
   return res.json()
 })
 
+//sets the innerHtml of body to the id of the created user
 .then(function(object){
   document.body.innerHTML = object["id"]
 })
 
+//sets the innerHTML of body to an error message (displays if request is unsuccessful)
 .catch(function(error){
   document.body.innerHTML = error.message
 })
